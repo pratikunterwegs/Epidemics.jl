3de2b9f56058e99eede9b8f2adca0488159c360a
Pratik Gupte <pratikgupte16@gmail.com>
2023-03-17 22:37:57 +0000

    Basic test for epidemic output as DF

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,15 @@
 using Epidemics
 using Test
 
+using DataFrames
+
 @testset "Epidemics.jl" begin
     x = 2
     y = 2
     @test Epidemics.sum_values(x,y) == 4
+end
+
+@testset "SEIR model" begin
+    data = Epidemics.epidemic()
+    @test typeof(data) == DataFrames.DataFrame
 end
