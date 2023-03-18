fc149dcbf57ecfaa1bf5d3c3d43127648e4b7972
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-03-18 15:19:56 +0000

    SEIR model with flexible age groups

--- src/seir.jl
+++ src/seir.jl
@@ -1,27 +1,24 @@
+"""
+    seir(du, u, p, t)
 
-# define function ODEs
+A simple SEIR epidemic model function that allows for multiple demographic
+    groups. This function is intended to be called internally from [`epidemic`](@ref).
+
+"""
 function seir(du, u, p, t)
-    S, E, I, R = u
+    # assumes that each element of the vector is a 
+    # vector of rates
+    β, β2, γ = p
 
-    # read beta, beta2, and gamma
-    β = @view p[1]
-    β2 = @view p[2]  ## WIP: Use NamedArrays to reduce confusion
-    γ = @view p[3]
-
-    # access the initial condition matrix
+    # view the values of each compartment per each age group
     S = @view u[:, 1]
     E = @view u[:, 2]
     I = @view u[:, 3]
     R = @view u[:, 4]
 
-    # change in compartments
-    dS = @view du[:,1]
-    dE = @view du[:,2]
-    dI = @view du[:,3]
-    dR = @view du[:,4]
-
-    @. du[:, 1] = -β * S * I
-    @. du[:, 2] = (β * S * I) - (β2 * E)
-    @. du[:, 3] = (β2 * E) - (γ * I)
+    # calculate change in compartment size
+    @. du[:, 1] = -β .* S .* I
+    @. du[:, 2] = β * S * I - (β2 * E)
+    @. du[:, 3] = β2 * E - γ * I
     @. du[:, 4] = γ * I
 end
