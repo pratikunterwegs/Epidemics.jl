a767577c2c6a84989b31ec91aa9115044770bead
Pratik Gupte <pratikgupte16@gmail.com>
2023-03-18 14:09:35 +0000

    Add helper fns, WIP using helpers in epidemic()

--- src/epidemic.jl
+++ src/epidemic.jl
@@ -3,15 +3,54 @@ include("seir.jl")
 
 using OrdinaryDiffEq
 using DataFrames
+using LinearAlgebra
+
+function prepare_contact_matrix(;contact_matrix, demography_vector)
+    # input checking here
+    # WIP check that square matrix has as many rows as demographic groups
+
+    # scale the contact matrix by its largest real eigenvalue
+    # eigvals is assumed to return only the real eigenvalues
+    # may need correction
+    contact_matrix = contact_matrix / maximum(LinearAlgebra.eigvals(contact_matrix))
+    
+    # scale by the demography, divide each row by corresponding demography
+    contact_matrix = contact_matrix ./ demography_vector
+    
+    return contact_matrix
+end
+
+function prepare_initial_conditions(;initial_conditions, demography_vector)
+    # input checking here; check that initial initial_conditions
+    # is a matrix and has as many rows as demography_vector
+
+    # rowwise multiplication of initial conditions by demography_vector
+    init = initial_conditions .* demography_vector
+
+    # return linearised values
+    return vec(init)
+end
 
 function epidemic(;
     model_name="seir", # named arguments begin here
     init=[1.0 - 1e-4, 1e-4, 0.0, 0.0],
+    contact_matrix, demography_vector,
     parameters=[0.1, 0.5, 0.05, 0.01],
     time_end=200.0,
     increment=0.1)
 
     # input checking goes here
+
+    # prepare the contact matrix
+    contact_matrix = prepare_contact_matrix(
+        contact_matrix=contact_matrix,
+        demography_vector=demography_vector
+    )
+
+    # prepare the initial conditions
+    init = prepare_initial_conditions(
+        initial_conditions=init, demography_vector=demography_vector
+    )
 
     # prepare the timespan
     timespan = (0.0, time_end)
