0f7a65a26beef4c3b38bc57bbf3854cab7302d90
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-03-18 15:21:01 +0000

    Minor edits to scratch file

--- src/test.jl
+++ src/test.jl
@@ -1,5 +1,4 @@
 
-a = 1
 using OrdinaryDiffEq
 
 include("seir.jl")
@@ -7,15 +6,13 @@ include("seir.jl")
 # Parameter
 # a vector of vectors of beta, beta2, and gamma
 parameters = [[0.14, 0.12, 0.2], [0.5, 0.5, 0.5], [0.05, 0.05, 0.05]]
-init = [0.9 0.9 0.9; 0.1 0.1 0.1; 0.0 0.0 0.0; 0.0 0.0 0.0]'
+init = [0.9 0.9 0.9; 0.09 0.09 0.09; 0.01 0.01 0.01; 0.0 0.0 0.0]'
 timespan = (0.0, 100)
 
 function Φ!(du, u, p, t)
     # assumes that each element of the vector is a 
     # vector of rates
-    β = @view p[1] # access transmission rates
-    β2 = @view p[2] # access transmission rates
-    γ = @view p[3] # acess recovery rates
+    β, β2, γ = p
 
     S = @view u[:, 1]
     E = @view u[:, 2]
@@ -27,7 +24,7 @@ function Φ!(du, u, p, t)
     dI = @view du[:, 3]
     dR = @view du[:, 4]
 
-    @. du[:, 1] = -β * S * I
+    @. du[:, 1] = -β .* S .* I
     @. du[:, 2] = β * S * I - (β2 * E)
     @. du[:, 3] = β2 * E - γ * I
     @. du[:, 4] = γ * I
