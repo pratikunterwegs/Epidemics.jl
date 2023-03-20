4edfb081157fa329b3bc28d81546f78a8cee88e7
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-03-20 13:43:49 +0000

    Helper fns to prepare contact matrix and epi parameters

--- src/helpers.jl
+++ src/helpers.jl
@@ -1,20 +1,23 @@
 
-function prepare_contact_matrix(;contact_matrix, demography_vector)
+using LinearAlgebra
+
+function prepare_contact_matrix(; contact_matrix, demography_vector)
     # input checking here
     # WIP check that square matrix has as many rows as demographic groups
 
     # scale the contact matrix by its largest real eigenvalue
     # eigvals is assumed to return only the real eigenvalues
     # may need correction
-    contact_matrix = contact_matrix / maximum(LinearAlgebra.eigvals(contact_matrix))
-    
+    contact_matrix = contact_matrix /
+                     maximum(LinearAlgebra.eigvals(contact_matrix))
+
     # scale by the demography, divide each row by corresponding demography
     contact_matrix = contact_matrix ./ demography_vector
-    
+
     return contact_matrix
 end
 
-function prepare_initial_conditions(;initial_conditions, demography_vector)
+function prepare_initial_conditions(; initial_conditions, demography_vector)
     # input checking here; check that initial initial_conditions
     # is a matrix and has as many rows as demography_vector
 
@@ -22,5 +25,17 @@ function prepare_initial_conditions(;initial_conditions, demography_vector)
     init = initial_conditions .* demography_vector
 
     # return linearised values
-    return vec(init)
+    return init
+end
+
+function r0_to_beta(; r0, infectious_period)
+    return r0 ./ (infectious_period)
+end
+
+function preinfectious_period_to_beta2(; preinfectious_period)
+    return 1.0 ./ preinfectious_period
+end
+
+function infectious_period_to_gamma(; infectious_period)
+    return 1.0 ./ infectious_period
 end
