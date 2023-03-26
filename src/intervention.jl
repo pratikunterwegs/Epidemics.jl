ef00c5be849502b1d2f0ee768ce0ada072b96a27
Pratik Gupte <pratikgupte16@gmail.com>
2023-03-26 20:47:12 +0100

    Add new Npi constructor

--- src/intervention.jl
+++ src/intervention.jl
@@ -1,5 +1,5 @@
 """
-  npi(time_begin, time_end, contact_reduction)
+  Npi(time_begin, time_end, contact_reduction)
 
 A structure to hold the end points and strength of a non-pharmaceutical
 intervention.
@@ -10,4 +10,13 @@ mutable struct Npi <: Intervention
   time_begin::Number
   time_end::Number
   contact_reduction::Vector{Number}
+end
+
+function Npi(; time_begin=50, time_end=80, contact_reduction=0.25)
+  # convert contact reduction to a vector if a single number
+  if length(contact_reduction) == 1
+    contact_reduction = [contact_reduction]
+  end
+
+  return Npi(time_begin, time_end, contact_reduction)
 end
