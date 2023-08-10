448d9f8b77df8a7eea74a873a82cbee9c6379cb5
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-08-10 15:45:30 +0100

    Fn prepare_args_default returns a tuple not dict

--- src/prepare_args_default.jl
+++ src/prepare_args_default.jl
@@ -5,6 +5,8 @@ A function to prepare the parameters passed to the default epidemic model.
 This function is intended to be called internally from
 [`epidemic_default`](@ref).
 
+## Named arguments
+
 The function expects the parameters argument to be a four element `tuple`
 with the following elements:
 - `population`, for the `population` affected by the epidemic;
@@ -12,6 +14,9 @@ with the following elements:
 - `intervention`, for any `Npi`s applied to the epidemic;
 - `vaccination`, for any `vaccination`s applied to the population;
     
+## Return
+
+Returns a `tuple` of the parameters for the default epidemic model.
 """
 function prepare_args_default(; population::Population, infection::Infection,
     intervention::Npi, vaccination::Vaccination)
@@ -21,12 +26,12 @@ function prepare_args_default(; population::Population, infection::Infection,
     )
 
     # prepare parameters
-    parameters = [
+    parameters = tuple(
         population,
         r0_to_beta(r0=infection.r0,
             infectious_period=infection.infectious_period
         ),
-        preinfectious_period_to_beta2(
+        preinfectious_period_to_alpha(
             preinfectious_period=infection.extra_arguments.preinfectious_period
         ),
         infectious_period_to_gamma(
@@ -34,7 +39,7 @@ function prepare_args_default(; population::Population, infection::Infection,
         ),
         intervention,
         vaccination
-    ]
+    )
 
     return parameters
 end
