07eaac6e20062f44824cf38194e92db25530709d
Pratik Gupte <pratikgupte16@gmail.com>
2023-08-15 21:38:35 +0100

    WIP cumulative NPI

--- src/intervention.jl
+++ src/intervention.jl
@@ -6,12 +6,21 @@ intervention.
     
 """
 mutable struct Npi
-    time_begin::Number
-    time_end::Number
-    contact_reduction::Vector
+  time_begin::Vector
+  time_end::Vector
+  contact_reduction::Matrix
 end
 
 function Npi(; time_begin::Number = 50, time_end::Number = 80,
              contact_reduction::Vector = [0.25])
-    return Npi(time_begin, time_end, contact_reduction)
+    # convert contact reduction to matrix
+    cr_matrix = reshape(contact_reduction, length(contact_reduction), 1)
+    return Npi(time_begin, time_end, cr_matrix)
 end
+
+# function for a cumulative intervention
+function cumulative_npi(;t, npi::Npi)
+  return npi
+end
+
+export Npi
