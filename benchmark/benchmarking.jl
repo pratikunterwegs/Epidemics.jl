967aa7be7f7d3923b7d9234e3a273c0fc58cef83
Pratik Gupte <pratikgupte16@gmail.com>
2023-08-15 20:37:32 +0100

    Remove benchmarking files

--- benchmark/benchmarking.jl
+++
@@ -1,8 +0,0 @@
-
-using BenchmarkTools
-using Epidemics
-
-time_end = 1000.0
-
-# benchmark the default model with 3 age groups, intervention, and vaccination
-@benchmark Epidemics.epidemic_default(time_end=time_end, increment=1.0)
