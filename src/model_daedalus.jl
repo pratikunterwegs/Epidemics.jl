aca4522ec97fe08ce07bf045b272e3f30f48ea1f
Pratik Gupte <pratikgupte16@gmail.com>
2024-12-02 14:49:05 +0000

    Update ODE RHS to remove vax strata

--- src/model_daedalus.jl
+++ src/model_daedalus.jl
@@ -61,19 +61,13 @@ function australia_initial_state(
         demography = australia_demography(), workers = aus_workers())
     p_infected = 1e-6
     p_susc = 1 - p_infected
-    compartments = 7
+    compartments = 8
     age_groups = 4
     econ_groups = 45
-    vaccine_strata = 2
 
-    init = [p_susc, 0.0, p_infected, 0.0, 0.0, 0.0, 0.0]
+    init = [p_susc, 0.0, p_infected, 0.0, 0.0, 0.0, 0.0, 0.0]
     init = reshape(init, 1, compartments)
     init = repeat(init, age_groups + econ_groups)
-
-    dummy = zeros(age_groups + econ_groups, compartments, vaccine_strata)
-
-    # assign to first layer
-    dummy[:, :, 1] = init
 
     # process demography to calculate non-working working age, and concat
     # demography and worker counts
@@ -84,7 +78,7 @@ function australia_initial_state(
     demography = [demography; workers]
 
     # multiply by demography - row i times element i
-    init = dummy .* demography
+    init = init .* demography
 
     return init
 end
@@ -142,13 +136,14 @@ function epidemic_daedalus_ode!(du, u, p, t)
 
     # view the values of each compartment per age group
     # rows represent age groups, epi compartments are columns
-    S = @view u[:, 1, :]
-    E = @view u[:, 2, :]
-    Is = @view u[:, 3, :]
-    Ia = @view u[:, 4, :]
-    H = @view u[:, 5, :]
-    R = @view u[:, 6, :]
-    D = @view u[:, 7, :]
+    S = @view u[:, 1]
+    E = @view u[:, 2]
+    Is = @view u[:, 3]
+    Ia = @view u[:, 4]
+    H = @view u[:, 5]
+    R = @view u[:, 6]
+    D = @view u[:, 7]
+    V = @view u[:, 8]
 
     # calculate new infections and re-infections
     community_infectious = sum(Is .+ Ia * epsilon, dims = 2)
@@ -158,34 +153,35 @@ function epidemic_daedalus_ode!(du, u, p, t)
     new_I = S .* foi
 
     # workplace
-    new_I_work = S[i_econ_groups, :] .* cw .*
-                 (Is[i_econ_groups, :] .+ (Ia[i_econ_groups, :] * epsilon))
+    new_I_work = S[i_econ_groups] .* cw .*
+                 (Is[i_econ_groups] .+ (Ia[i_econ_groups] * epsilon))
 
-    new_I[i_econ_groups, :] += new_I_work
+    new_I[i_econ_groups] += new_I_work
 
     # views to the change array slice
-    dS = @view du[:, 1, :]
-    dE = @view du[:, 2, :]
-    dIs = @view du[:, 3, :]
-    dIa = @view du[:, 4, :]
-    dH = @view du[:, 5, :]
-    dR = @view du[:, 6, :]
-    dD = @view du[:, 7, :]
+    dS = @view du[:, 1]
+    dE = @view du[:, 2]
+    dIs = @view du[:, 3]
+    dIa = @view du[:, 4]
+    dH = @view du[:, 5]
+    dR = @view du[:, 6]
+    dD = @view du[:, 7]
+    dV = @view du[:, 8]
 
     # new vaccinations from susceptibles
-    new_Svax = @view S[:, 1]
-    new_Swane = @view S[:, 2]
+    # new_Svax = @view S[:, 1]
+    # new_Swane = @view S[:, 2]
 
     # new vaccinations from recovered
-    new_Rvax = @view R[:, 1]
-    new_Rwane = @view R[:, 2]
+    # new_Rvax = @view R[:, 1]
+    # new_Rwane = @view R[:, 2]
 
     # calculate change in compartment size and update the change matrix dU
     # note the use of @. for broadcasting, equivalent to .=
     # change in susceptibles
-    @. dS = -new_I + (rho * R)
-    @. dS[:, 1] += (-new_Svax * (nu * switch) + new_Swane * psi)
-    @. dS[:, 2] += (new_Svax * (nu * switch) - new_Swane * psi)
+    @. dS = -new_I + (rho * R) - (nu * switch * S) + (psi * V)
+    # @. dS[:, 1] += (-new_Svax * (nu * switch) + new_Swane * psi)
+    # @. dS[:, 2] += (new_Svax * (nu * switch) - new_Swane * psi)
 
     # change in exposed
     @. dE = new_I - (sigma * E)
@@ -205,13 +201,15 @@ function epidemic_daedalus_ode!(du, u, p, t)
 
     # change in recovered
     @. dR = (gamma_Ia * Ia) + (gamma_Is * Is) +
-            (gamma_H .* H) - (rho * R)
+            (gamma_H .* H) - (rho + (nu * switch) * R)
 
-    @. dR[:, 1] += (-new_Rvax * (nu * switch) + new_Rwane * psi)
-    @. dR[:, 2] += (new_Rvax * (nu * switch) - new_Rwane * psi)
+    # @. dR[:, 1] += (-new_Rvax * (nu * switch) + new_Rwane * psi)
+    # @. dR[:, 2] += (new_Rvax * (nu * switch) - new_Rwane * psi)
 
     # change in dead
     @. dD = omega .* H
+
+    @. dV = nu * (S + R) - (psi * V)
 end
 
 """
